bf03948fd1f19d040097e4d9ee009399a0c4db4a
you-5805 <you.iwamoto918@gmail.com>
2021-01-08 00:26:18 +0900

    取得データ整形

--- app/controllers/api/v1/community_centers_controller.rb
+++ app/controllers/api/v1/community_centers_controller.rb
@@ -6,12 +6,12 @@ class Api::V1::CommunityCentersController < ApiController
 
   def show
     @community_center = CommunityCenter.find_by(user_id: params[:id])
-    render json: @community_center
+    render 'show'
   end
 
   def page
     @community_center = CommunityCenter.find_by(community_center_id: params[:id])
-    render json: @community_center
+    render 'show'
   end
   
   def create
---
+++ app/views/api/v1/community_centers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @community_center, :name, :user_id, :community_center_id
