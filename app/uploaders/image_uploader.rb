4f42d1476fa0e04b4b6be672eb8c355a88384439
you-5805 <you.iwamoto918@gmail.com>
2021-01-12 15:31:56 +0900

    修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,16 +9,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  def file_name
-    "#{secure_token}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-
-    def secure_token
-      var = :"@#{mounted_as}_secure_token"
-      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
-    end
-
 end
