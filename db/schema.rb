692a1c2728381df98fa3669f7fb0c3ef43d743b4
you-5805 <you.iwamoto918@gmail.com>
2021-02-06 08:54:16 +0900

    favicon修正、mute_notificationを追加

--- db/schema.rb
+++ db/schema.rb
@@ -133,7 +133,8 @@ ActiveRecord::Schema.define(version: 2021_02_05_135622) do
     t.string "remember_digest"
     t.string "activation_digest"
     t.string "authentication_digest"
-    t.boolean "activated", default: false
+    t.boolean "activated", default: false, null: false
+    t.boolean "mute_notification", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
